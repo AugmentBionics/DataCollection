8d89390e1708151096205ee953d99cc892a3a919
ameertg <ameertg@users.noreply.github.com>
2018-10-10 18:12:53 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DataCollection
 Fetch data for machine learning from myo sensor
+Data is read at 10 ms intervals
+File naming protocol:
+- first number of file name corresponds to red sensor position (centered[1] or offset to the left[2])
+- second number corresponds to blue sensor position (directly above elbow-crease [1], end of bicep [2])
+- third number gives black sensor position (inner bone[1], outer bone[2])
--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import serial
 import csv
+from sys import argv
+from time import sleep
 
-file = open('muscleRead.csv', 'wb')
-wr = csv.writer(file)
+file1 = open('{}_{}_{}_v1.csv'.format(argv[1], argv[2], argv[3]), 'wb')
+file2 = open('{}_{}_{}_v2.csv'.format(argv[1], argv[2], argv[3]), 'wb')
 
-port = serial.Serial("/dev/tty.usbmodem1411",9600)
+
+port = serial.Serial("/dev/tty.usbmodem1421",9600)
 data = []
 flex_time = []
+
+wr = csv.writer(file1)
 
 while(len(data)<1400):
     if len(data) >= 500 and len(data) <= 1000:
@@ -18,8 +23,28 @@ while(len(data)<1400):
         print("STOP")
         
     data.append(port.readline())
+    sleep(0.01)
+    
+wr.writerow(data)
+wr.writerow(flex_time)
+file1.close()
 
+
+data = []
+flex_time = []
+wr = csv.writer(file2)
+while(len(data)<1400):
+    if len(data) >= 500 and len(data) <= 1000:
+        flex_time.append('Flex')
+        print("FLEX")
+        
+    else:
+        flex_time.append('Not')
+        print("STOP")
+        
+    data.append(port.readline())
+    sleep(0.01)
 
 wr.writerow(data)
 wr.writerow(flex_time)
-file.close()
+file2.close()
